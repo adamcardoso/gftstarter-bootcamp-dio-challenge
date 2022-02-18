7c0d62a7bee25e35a08f1412ca4ca01b0db1fc7d
Adam Cardoso <adamjuniorcardoso@gmail.com>
2022-02-18 16:12:21 -0300

    first commit

---
+++ src/main/java/com/challenge/userdeptarment/UserdeptarmentApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.userdeptarment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserdeptarmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserdeptarmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/challenge/userdeptarment/UserdeptarmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.userdeptarment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserdeptarmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
