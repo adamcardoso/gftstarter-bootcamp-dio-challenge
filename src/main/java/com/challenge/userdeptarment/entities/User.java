1b5ddc15d014c2bf104afe50911b64c6b0070d16
Adam Cardoso <adamjuniorcardoso@gmail.com>
2022-02-18 16:26:34 -0300

    Creating User and Department

---
+++ src/main/java/com/challenge/userdeptarment/entities/User.java
@@ -0,0 +1,45 @@
+package com.challenge.userdeptarment.entities;
+
+public class User {
+    private Long id;
+    private String userName;
+    private String email;
+
+    private Departament departament; // association
+
+    public User(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Departament getDepartament() {
+        return departament;
+    }
+
+    public void setDepartament(Departament departament) {
+        this.departament = departament;
+    }
+}
